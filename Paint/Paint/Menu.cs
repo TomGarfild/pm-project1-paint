2d260f0b4183e8f93b488658f62aba71fc7a79d3
Oleksii Safroniuk <osafroniuk@gmail.com>
2021-01-14 15:20:30 +0200

    Created menu and add some context

---
+++ Paint/Paint/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Paint
+{
+    public class Menu
+    {
+        public void Start()
+        {
+            Console.WriteLine("Hello, in Console Paint");
+            while (true)
+            {
+                var command = GetInputCommand(new string[]
+                    {"Scenes", "Draw", "Arrange shapes", "Exit"});
+                switch (command)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        //Exit from application
+                        return;
+
+                }
+            }
+        }
+        private static int GetInputCommand(string[] commands)
+        {
+            Console.WriteLine("   Menu");
+            for (int i = 0; i < commands.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {commands[i]}");
+            }
+
+            Console.WriteLine("Enter number of command in menu");
+            int command;
+            while (!int.TryParse(Console.ReadLine(), out command) || command < 1 || command > commands.Length)
+            {
+                Console.WriteLine("\nWrong command\n");
+                Console.WriteLine("   Menu");
+                for (int i = 0; i < commands.Length; i++)
+                {
+                    Console.WriteLine($"\t{i + 1}. {commands[i]}");
+                }
+
+                Console.WriteLine("Enter number of command in menu");
+            }
+
+            return command;
+        }
+    }
+}
