fa1abb8b81f0f73cdd0f293b7689f84c216c41a2
Oleksii Safroniuk <osafroniuk@gmail.com>
2021-01-15 20:28:41 +0200

    Change menu, scenes

--- Paint/Paint/Scene.cs
+++ Paint/Paint/Scene.cs
@@ -2,6 +2,12 @@
 {
     public class Scene
     {
-        
+        public string Name { get; }
+        public int Id { get; }
+        public Scene(string name)
+        {
+            Name = name;
+            Id = 1;
+        }
     }
 }
