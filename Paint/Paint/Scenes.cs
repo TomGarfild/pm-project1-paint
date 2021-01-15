3ef95d5882edee501929b9c11031364d4bb48ec7
Oleksii Safroniuk <osafroniuk@gmail.com>
2021-01-15 18:43:28 +0200

    Add scenes

---
+++ Paint/Paint/Scenes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Paint
+{
+    public class Scenes
+    {
+        private List<Scene> scenes;
+
+        public Scenes()
+        {
+            try
+            {
+                var json = File.ReadAllText("scenes.json");
+                scenes = JsonSerializer.Deserialize<List<Scene>>(json);
+            }
+            catch (Exception)
+            {
+                scenes = new List<Scene>();
+            }
+        }
+
+        public void Add(Scene scene)
+        {
+            scenes.Add(scene);
+        }
+    }
+}
