ed1bbb17b98dfb55faa07c19af1d2ebadf9c3909
Oleksii Safroniuk <osafroniuk@gmail.com>
2021-01-19 12:20:25 +0200

    Add get height and width for rectangle

--- Paint/Paint/Shapes/Rectangle.cs
+++ Paint/Paint/Shapes/Rectangle.cs
@@ -1,12 +1,25 @@
-﻿namespace Paint.Shapes
+﻿using System;
+
+namespace Paint.Shapes
 {
     public class Rectangle : Shape
     {
         public bool Filled { get; }
+        public int Width { get; }
+        public int Height { get; }
         public Rectangle(int depth, bool filled)
             : base(depth)
         {
             Filled = filled;
+            Width = GetSide("width");
+            Height = GetSide("height");
+        }
+        private int GetSide(string type)
+        {
+            Console.Write($"Enter {type} of your rectangle: ");
+            if (int.TryParse(Console.ReadLine(), out var size)
+                && size >= 1) return size;
+            return 1;
         }
 
         protected override int CalculateSquare()
